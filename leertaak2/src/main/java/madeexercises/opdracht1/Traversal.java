39ab53be966caf5d16150bff80369df14b10dbe2
Johan <j.kladder@st.hanze.nl>
2016-02-15 16:16:02 +0100

    ADD: Treenode

---
+++ leertaak2/src/main/java/madeexercises/opdracht1/Traversal.java
@@ -0,0 +1,15 @@
+package madeexercises.opdracht1;
+
+/**
+ * Created by johankladder on 15-2-16.
+ */
+public class Traversal {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean inOrderTraversal() {
+        return false;
+    }
+}
---
+++ leertaak2/src/main/java/madeexercises/opdracht1/TreeNode.java
@@ -0,0 +1,15 @@
+package madeexercises.opdracht1;
+
+/**
+ * Created by johankladder on 15-2-16.
+ */
+public class TreeNode {
+
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
