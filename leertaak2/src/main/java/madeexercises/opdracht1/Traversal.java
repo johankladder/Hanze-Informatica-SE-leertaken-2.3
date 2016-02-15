864168befa14966066d7c66d6f8f4fd64bf18003
Johan <j.kladder@st.hanze.nl>
2016-02-15 16:41:54 +0100

    ADD: Init binary tree based on predefined structure

--- leertaak2/src/main/java/madeexercises/opdracht1/Traversal.java
+++ leertaak2/src/main/java/madeexercises/opdracht1/Traversal.java
@@ -17,9 +17,36 @@ public class Traversal {
         rootNode = initBinaryTree();
     }
 
-    // TODO: Create example-tree to perform test on
+    /*
+    Creates a binary tree based on the structure mentioned in the
+    resources. Will return the rootNode if the tree when done
+    initialising.
+     */
     private TreeNode initBinaryTree() {
-        return null;
+        // Nodes: // For visual, check resource!
+        TreeNode root = new TreeNode(50);
+        TreeNode r1 = new TreeNode(20);
+        TreeNode r2 = new TreeNode(10);
+        TreeNode r3 = new TreeNode(25);
+        TreeNode r4 = new TreeNode(5);
+
+        TreeNode r5 = new TreeNode(70);
+        TreeNode r6 = new TreeNode(40);
+        TreeNode r7 = new TreeNode(85);
+        TreeNode r8 = new TreeNode(30);
+
+        // Relations (Parent and child):
+        root.left = r1;
+        r1.left = r2;
+        r1.right = r3;
+        r2.left = r4;
+
+        root.right = r5;
+        r5.left = r6;
+        r5.right = r7;
+        r7.left = r8;
+
+        return root;
     }
 
     // TODO: Create in order traversal on created binary tree
