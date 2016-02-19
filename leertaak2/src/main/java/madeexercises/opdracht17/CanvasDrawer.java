2c17067f7be8806df4944ac46596248208f25c9a
Johan <j.kladder@st.hanze.nl>
2016-02-19 16:06:14 +0100

    ADD: logic

--- leertaak2/src/main/java/madeexercises/opdracht17/CanvasDrawer.java
+++ leertaak2/src/main/java/madeexercises/opdracht17/CanvasDrawer.java
@@ -2,8 +2,7 @@ package madeexercises.opdracht17;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import madeexercises.classifier.classifier.Node;
 
 /**
  * Created by johankladder on 19-2-16.
@@ -20,47 +19,25 @@ public class CanvasDrawer {
     }
 
     private void init() {
-        canvas.setHeight(500);
-        canvas.setWidth(500);
+        canvas.setHeight(700);
+        canvas.setWidth(1000);
         orW = canvas.getWidth();
         orH = canvas.getHeight();
-
-        final GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-        int y = 10; // Space between circels:
-        double lastCenter  = canvas.getWidth()/2;
-        boolean first = true;
-        double middell = 0;
-        double middelr = 0;
-        for(int i= 0; i < 7; i++) {
-            boolean left = true;
-            for(int j = 0; j < i; j++) {
-                if(first) {
-                    gc.strokeOval(lastCenter, y, 5, 5);
-                    middell = lastCenter - (lastCenter * 0.75);
-                    middelr = lastCenter + (lastCenter * 0.75);
-                    first = false;
-                } else {
-                    if(left) {
-                        gc.strokeOval(middell, y, 5, 5);
-                        left = false;
-                        middell = middell - (middell * 0.75);
-                    } else {
-                        gc.strokeOval(middelr, y, 5, 5);
-                        left = true;
-                        middelr = middelr + (middelr * 0.75);
-                    }
-                }
-            }
-            //lastCenter =
-            y = y + 50;
-            //lastCenter = lastCenter * 2;
-
-        }
-
-
-
     }
+
+    public void draw(Node node) {
+        drawNode(node, canvas.getWidth(), 0);
+    }
+
+    public void drawNode(Node node, double width, int lastY) {
+        if(node != null) {
+            double v = width / 2;
+            final GraphicsContext gc = canvas.getGraphicsContext2D();
+            gc.strokeOval(v, lastY + 70, 20,20);
+            drawNode((Node) node.getChild().get("1"), v, lastY + 70);
+        }
+    }
+
+
 
 }
--- leertaak2/src/main/java/madeexercises/opdracht17/GuiLauncher.java
+++ leertaak2/src/main/java/madeexercises/opdracht17/GuiLauncher.java
@@ -5,11 +5,17 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import madeexercises.classifier.classifier.ClassifierModel;
+
+import java.io.IOException;
 
 /**
  * Created by johankladder on 19-2-16.
  */
 public class GuiLauncher extends Application {
+
+    private ClassifierModel model;
+    private CanvasDrawer drawer;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -20,7 +26,26 @@ public class GuiLauncher extends Application {
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
 
-        CanvasDrawer drawer = new CanvasDrawer(canvas);
+        drawer = new CanvasDrawer(canvas);
         primaryStage.show();
+        initt();
+    }
+
+    public void initt() throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    model = new ClassifierModel();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+
+        drawer.draw(model.getRoot());
+
+
     }
 }
