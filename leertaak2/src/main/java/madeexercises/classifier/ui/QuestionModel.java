ba384abd988996a80e27a1581888c73688b142ae
Kevin <k.haitsma@inventief-it.nl>
2016-02-19 12:58:01 +0100

    init opdr16

--- leertaak2/src/main/java/madeexercises/classifier/ui/QuestionModel.java
+++ leertaak2/src/main/java/madeexercises/classifier/ui/QuestionModel.java
@@ -7,13 +7,20 @@ import madeexercises.classifier.classifier.Node;
  */
 public class QuestionModel {
     Node root;
+    Node currentNode;
 
     public QuestionModel() {
     }
 
     public void setRoot(Node root){
         this.root = root;
-        System.out.println(root);
     }
 
+    public void getAnswer(Node node, Boolean answer) {
+        currentNode = node;
+    }
+
+    public Node getCurrentNode() {
+        return currentNode;
+    }
 }
