927447bfd1550cd03182ddbd8396683986df1791
Johan <j.kladder@st.hanze.nl>
2016-02-19 13:07:15 +0100

    ADD: INIT VIEW

---
+++ leertaak2/src/main/java/madeexercises/classifier/ui/QuestionController.java
@@ -0,0 +1,39 @@
+package madeexercises.classifier.ui;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+/**
+ * Created by johankladder on 19-2-16.
+ */
+public class QuestionController extends BorderPane implements View{
+
+    private QuestionModel model;
+    private Label trueLabel = new Label("True");
+    private Label falseLabel = new Label("False");
+
+    private CheckBox trueCheckBox = new CheckBox();
+    private CheckBox falseCheckBox = new CheckBox();
+
+    public QuestionController(QuestionModel model) {
+        this.model = model;
+        init();
+    }
+
+    private void init() {
+        GridPane gridPane = new GridPane();
+        gridPane.add(trueLabel, 0, 0);
+        gridPane.add(falseLabel, 1, 0);
+
+        gridPane.add(trueCheckBox, 0, 1);
+        gridPane.add(falseCheckBox, 1, 1);
+        setCenter(gridPane);
+    }
+
+    public void updateView() {
+        trueCheckBox.setSelected(false);
+        falseCheckBox.setSelected(false);
+    }
+}
---
+++ leertaak2/src/main/java/madeexercises/classifier/ui/View.java
@@ -0,0 +1,9 @@
+package madeexercises.classifier.ui;
+
+/**
+ * Created by johankladder on 19-2-16.
+ */
+public interface View {
+
+    void updateView();
+}
